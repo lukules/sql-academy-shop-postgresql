5532e313b7b5ee4ef878d0be43643dc541ff0b91
Łukasz Kulesza <kulesza.lukasz@icloud.com>
2026-08-27 15:27:51 +0200

    Zadanie 03 - Posortuj produkty malejąco po cenie, a przy tej samej cenie alfabetycznie

--- sql_academy_shop.sql
+++ sql_academy_shop.sql
@@ -1,4 +1,8 @@
+
 SELECT first_name, last_name, email, city FROM customers WHERE country = 'Polska';
+
+SELECT * FROM customers WHERE country = 'Polska';
+
 
 SELECT DISTINCT country FROM customers; 
 
