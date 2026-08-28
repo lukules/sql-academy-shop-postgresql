d75f4d495b55aa15b7785530c11e964312e3866b
Łukasz Kulesza <kulesza.lukasz@icloud.com>
2026-08-28 10:43:17 +0200

    Zadanie 07 - Pokaż osobno klientów bez telefonu i zamówienia z datą wysyłki

--- sql_academy_shop.sql
+++ sql_academy_shop.sql
@@ -1,22 +1,22 @@
 -- Zadanie 01 - Wyświetl aktywnych klientów z Polski.
 -- Oczekiwany wynik: Imię, nazwisko, e-mail i miasto.
-SELECT first_name, last_name, email, city FROM customers WHERE country = 'Polska';
+SELECT first_name, last_name, email, city FROM academy.customers WHERE country = 'Polska';
 
 
 -- Zadanie 02 - Pokaż unikalne kraje klientów.
 -- Oczekiwany wynik: Jedna kolumna bez powtórzeń.
-SELECT DISTINCT country FROM customers; 
+SELECT DISTINCT country FROM academy.customers; 
 
 
 -- Zadanie 03 - Posortuj produkty malejąco po cenie, a przy tej samej cenie alfabetycznie.
 -- Oczekiwany wynik: Nazwa, cena i stan magazynowy.
-SELECT product_name, unit_price, stock_quantity FROM products
+SELECT product_name, unit_price, stock_quantity FROM academy.products
 ORDER BY 1,2;
 
 
 -- Zadanie 04 - Znajdź produkty droższe niż 500 zł z kategoriami 1 lub 2.
 -- Oczekiwany wynik: Produkty spełniające oba kryteria.
-SELECT product_name, category_id, unit_price FROM products
+SELECT product_name, category_id, unit_price FROM academy.products
 WHERE unit_price > 500 AND (category_id = 1 OR category_id = 2)
 
 
@@ -25,9 +25,13 @@ WHERE unit_price > 500 AND (category_id = 1 OR category_id = 2)
 SELECT product_id, product_name FROM academy.products WHERE NOT discontinued;
 
 
--- Zadnaie 06 - Pokaż pięć najnowszych zamówień.
+-- Zadanie 06 - Pokaż pięć najnowszych zamówień.
 -- Oczekiwany wynik: Pięć rekordów od najnowszego.
-SELECT * FROM orders;
+SELECT order_id, order_date FROM academy.orders
+ORDER BY order_date DESC 
+LIMIT 5;
 
-
+-- Zadanie 07 - Pokaż osobno klientów bez telefonu i zamówienia z datą wysyłki.
+-- Oczekiwany wynik: Dwa wyniki
+SELECT * FROM academy.clients;
 
